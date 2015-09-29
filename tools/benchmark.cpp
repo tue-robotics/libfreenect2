b5e8d0f4006d1251f7fe0d295261a128c076e2a4
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-09-29 17:56:48 +0200

    Also check validity of output

--- tools/benchmark.cpp
+++ tools/benchmark.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <fstream>
 
+#include <cmath>
+
 //#include <opencv2/highgui/highgui.hpp>
 
 // ----------------------------------------------------------------------------------------------------
@@ -57,8 +59,6 @@ public:
 
         p0_table_ = new unsigned char[p0_table_size_];
         file_in_.read(reinterpret_cast<char*>(p0_table_), p0_table_size_);
-
-        std::cout << "P0 table: " << p0_table_size_ << std::endl;
 
         return true;
     }
@@ -140,12 +140,17 @@ int main(int argc, char **argv)
 
     Listener listener;
 
+    libfreenect2::DepthPacketProcessor::Config config;
+    config.EnableBilateralFilter = false;
+    config.EnableEdgeAwareFilter = false;
+
     libfreenect2::CpuDepthPacketProcessor processor_orig;
     processor_orig.load11To16LutFromFile("11to16.bin");
     processor_orig.loadXTableFromFile("xTable.bin");
     processor_orig.loadZTableFromFile("zTable.bin");
     processor_orig.loadP0TablesFromCommandResponse(reader.p0_table(), reader.p0_table_size());
     processor_orig.setFrameListener(&listener);
+    processor_orig.setConfiguration(config);
 
     libfreenect2::fast::CpuDepthPacketProcessor processor_fast;
     processor_fast.load11To16LutFromFile("11to16.bin");
@@ -153,23 +158,61 @@ int main(int argc, char **argv)
     processor_fast.loadZTableFromFile("zTable.bin");
     processor_fast.loadP0TablesFromCommandResponse(reader.p0_table(), reader.p0_table_size());
     processor_fast.setFrameListener(&listener);
+    processor_fast.setConfiguration(config);
 
     libfreenect2::DepthPacket packet;
     packet.buffer = 0;
 
     while (reader.nextPacket(packet))
     {       
+        std::cout << "----------------------------------------" << std::endl;
+        std::cout << "Frame " << packet.sequence << std::endl << std::endl;
+
+        std::cout << "Performance:" << std::endl;
+
         Timer t;
         processor_orig.process(packet);
-        std::cout << "Original processing took: " << t.getElapsedTimeInMilliSec() << " ms" << std::endl;
+        std::cout << "    Original processing took: " << t.getElapsedTimeInMilliSec() << " ms" << std::endl;
+        libfreenect2::Frame* depth_frame_orig = listener.depth_frame;
 
         Timer t2;
         processor_fast.process(packet);
-        std::cout << "Fast processing took:     " << t2.getElapsedTimeInMilliSec() << " ms" << std::endl;
+        std::cout << "    Fast processing took:     " << t2.getElapsedTimeInMilliSec() << " ms" << std::endl;
+        libfreenect2::Frame* depth_frame_fast = listener.depth_frame;
+
+        float* f_orig = reinterpret_cast<float*>(depth_frame_orig->data);
+        float* f_fast = reinterpret_cast<float*>(depth_frame_fast->data);
+
+        float max_diff = 0;
+        unsigned int num_false_positives = 0;
+        unsigned int num_false_negatives = 0;
+
+        unsigned int frame_size = depth_frame_orig->width * depth_frame_orig->height;
+        for(unsigned int i = 0; i < frame_size; ++i)
+        {
+            if (*f_orig != 0 && *f_fast == 0)
+                ++num_false_negatives;
+            else if (*f_orig == 0 && *f_fast != 0)
+                ++num_false_positives;
+            else
+            {
+                float diff = std::abs(*f_orig - *f_fast);
+                max_diff = std::max(max_diff, diff);
+            }
+
+            ++f_orig;
+            ++f_fast;
+        }
+
+        std::cout << std::endl;
+        std::cout << "Validity:" << std::endl;
+        std::cout << "    Max difference  = " << max_diff / 1000 << " meter" << std::endl;
+        std::cout << "    False positives = " << (float)num_false_positives / frame_size << " % of pixels" << std::endl;
+        std::cout << "    False negatives = " << (float)num_false_negatives / frame_size << " % of pixels" << std::endl;
 
         std::cout << std::endl;
 
-//        cv::Mat canvas(listener.depth_frame->height, listener.depth_frame->width, CV_32FC1, listener.depth_frame->data);
+//        cv::Mat canvas(depth_frame_orig->height, depth_frame_orig->width, CV_32FC1, depth_frame_orig->data);
 //        cv::imshow("depth", canvas / 5000);
 //        cv::waitKey();
     }
